df12199d978f132b167cebc36824babdb7d7bec3
manalinaik20 <naikmanali700@gmail.com>
2025-06-13 17:18:39 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting Python HTTP server..."
 cd ~/codedeploy-demo
-nohup python3 -m http.server 80 > /tmp/web.log 2>&1 &
+nohup python3 -m http.server 8080 > /tmp/web.log 2>&1 &
